bf6e27648e92e460bf8a88d3b437e4ac7bd79f5f
SjorsHoek <S.hoek@geogap.nl>
2020-02-17 22:21:08 +0100

    perfomance increase

--- src/app/map-container/map-container.component.ts
+++ src/app/map-container/map-container.component.ts
@@ -20,29 +20,30 @@ export class MapContainerComponent implements OnInit, AfterViewInit {
       let activeClassName = 'active-d3-item';
       let circleradius = 50;
       let svg = select('body').select('#frontcover').attr('class', 'fullsize');
-  
+
       let dragslistner = drag()
         .on('start', dragstarted)
         .on('drag', () => {
           selectAll("circle")
-          .style('left', (event.x - circleradius) + 'px')
-          .style('top', (event.y - circleradius) + 'px');
-  
-        let screencoordinates = [event.x, event.y] as [number, number];
-        this.mapInteractionService.queryCoordinates.next(screencoordinates)
+            .style('left', (event.x - circleradius) + 'px')
+            .style('top', (event.y - circleradius) + 'px');
+
+          let screencoordinates = [event.x, event.y] as [number, number];
+          this.mapInteractionService.queryCoordinates.next(screencoordinates)
         })
         .on('end', dragended);
-  
+
       svg.call(dragslistner);
-  
+
       function dragstarted() {
         let coords = mouse(this);
         svg.append("circle")
           .attr("class", 'circle-follow active-d3-item')
           .style('left', (coords[0] - circleradius) + 'px')
           .style('top', (coords[1] - circleradius) + 'px');
+        
       }
-  
+
       function dragended() {
         selectAll('circle').classed(activeClassName, false);
         selectAll('circle').remove()
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { Map } from 'mapbox-gl';
 import mapstyle from '../../assets/mapstyle.json';
 import { MapInteractionService } from '../shared/map-interaction.service';
@@ -13,45 +13,48 @@ import { filter } from 'rxjs/operators';
 export class MapComponent implements OnInit {
   public map: Map;
 
-  constructor(private mapInteractionService: MapInteractionService, private speechService: SpeechService) { 
+  constructor(private mapInteractionService: MapInteractionService, private speechService: SpeechService, private zone: NgZone) { 
   }
 
   ngOnInit(): void {
-    this.map = new Map({
-      container: 'map',
-      style: mapstyle as any,
-      hash: true,
-      zoom: 16,
-      pitch: 0,
-      bearing: -15.7,
-      center: [5.24574, 51.81254],
-      attributionControl: false
-    });
-
-    this.mapInteractionService.queryCoordinates.pipe(
-      filter(coords => coords !== null)
-    ).subscribe(coords => {
-      const features = this.map.queryRenderedFeatures([
-        [coords[0]-2, coords[1]-2],
-        [coords[0]+2, coords[1]+2],
-      ]
-      );
-      if (features === undefined || features === null || features.length === 0) {
-        return;
-      }
-
-      const feature = features[0];
-      let words = ''
-      if (feature.sourceLayer === 'roads') {
-        words = `road ${feature.properties.name}`
-      } else if (feature.sourceLayer === 'water' && feature.properties.type === 'water_way') {
-        words = `${feature.sourceLayer}`;
-      } else {
-        words = `${feature.sourceLayer} ${feature.properties.type} ${feature.properties.subtype || ''}`;
-      }
-
-      this.speechService.wordsToUtter.next(words);
+    this.zone.runOutsideAngular(() => {
+      this.map = new Map({
+        container: 'map',
+        style: mapstyle as any,
+        hash: true,
+        zoom: 16,
+        pitch: 0,
+        bearing: -15.7,
+        center: [5.24574, 51.81254],
+        attributionControl: false
+      });
+  
+      this.mapInteractionService.queryCoordinates.pipe(
+        filter(coords => coords !== null)
+      ).subscribe(coords => {
+        const features = this.map.queryRenderedFeatures([
+          [coords[0]-2, coords[1]-2],
+          [coords[0]+2, coords[1]+2],
+        ]
+        );
+        if (features === undefined || features === null || features.length === 0) {
+          return;
+        }
+  
+        const feature = features[0];
+        let words = ''
+        if (feature.sourceLayer === 'roads') {
+          words = `road ${feature.properties.name}`
+        } else if (feature.sourceLayer === 'water' && feature.properties.type === 'water_way') {
+          words = `${feature.sourceLayer}`;
+        } else {
+          words = `${feature.sourceLayer} ${feature.properties.type} ${feature.properties.subtype || ''}`;
+        }
+  
+        this.speechService.wordsToUtter.next(words);
+      })
     })
+
 
 
 
