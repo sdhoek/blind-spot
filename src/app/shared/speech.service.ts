d694c502d598d72d7fa7bcb108a5d5ee046fe65e
SjorsHoek <S.hoek@geogap.nl>
2020-02-17 21:11:41 +0100

    remove welcome message

--- src/app/shared/speech.service.ts
+++ src/app/shared/speech.service.ts
@@ -7,14 +7,15 @@ import { distinctUntilChanged } from 'rxjs/operators';
 export class SpeechService {
   public wordsToUtter = new Subject();
   public voices: SpeechSynthesisVoice[] = [];
+  
 
   constructor() {
 
     this.setup();
-    speechSynthesis.speak(new SpeechSynthesisUtterance("Listen"));
-    setTimeout(() => {
-      speechSynthesis.speak(new SpeechSynthesisUtterance("to the world"));
-    }, 1500)
+    // speechSynthesis.speak(new SpeechSynthesisUtterance("Listen"));
+    // setTimeout(() => {
+    //   speechSynthesis.speak(new SpeechSynthesisUtterance("to the world"));
+    // }, 1500)
     
   }
 
@@ -29,7 +30,7 @@ export class SpeechService {
     speechSynthesis.cancel();
     this.voices = speechSynthesis.getVoices();
     const utterance = new SpeechSynthesisUtterance(words)
-    utterance.voice = this.voices[3];
+    utterance.voice = this.voices[5];
     speechSynthesis.speak(utterance);
   }
 
