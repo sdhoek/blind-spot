52ec2e4e2b2acb92df41540c83ddbcd31874bb63
NieneB <niene.boeijen@gmail.com>
2020-02-17 20:04:23 +0100

    map and circle cover in app

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import mapstyle from '../../assets/mapstyle.json';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
+
+  let map = new mapboxgl.Map({
+    container: 'map',
+    style: mapstyle,
+    hash: true,
+    zoom: 14.5,
+    pitch: 0,
+    bearing: -15.7,
+    center: [5.24574, 51.81254],
+    attributionControl:false
+  });
+
   }
 
 }
+
+
